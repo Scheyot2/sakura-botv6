fa440d2608af40e5db03d90d7afdc0913406dac0
seunome <you@example.com>
2022-12-16 15:39:07 -0300

    First commit

--- dono/gitdobot.js
+++ dono/gitdobot.js
@@ -1,6 +1,6 @@
 const gitdobot = (prefix) => {
 return`
-*SÓ NO CANAL DO MEU CRIADOR ^-^ LÁ TEM O VÍDEO, O LINK DO BOT TA NA DESCRIÇÃO:) https://youtube.com/channel/UCPEVqKFH45x4grF2qk3fldw*
+*SÓ NO CANAL DO MEU CRIADOR ^-^ LÁ TEM O VÍDEO, O LINK DO BOT TA NOS COMENTÁRIOS FIXADO :) https://youtube.com/@MarcioScheyot*
 `
 }
 
